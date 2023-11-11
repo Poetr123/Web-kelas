db3408119e4905377432610b63e6b848d489b72b
Poetr123 <150533614+Poetr123@users.noreply.github.com>
2023-11-11 23:33:45 +0700

    Update backButton.js

--- backButton.js
+++ backButton.js
@@ -1,10 +1,15 @@
-// backButton.js
 document.addEventListener('DOMContentLoaded', function() {
-    const backButton = document.createElement('button');
-    backButton.textContent = 'Back';
-    backButton.addEventListener('click', function() {
-        history.back();
-    });
+    const popup = document.getElementById('popup');
 
-    document.body.appendChild(backButton);
+    function changeColor() {
+        const colors = ['#ff8adb', '#8affa5', '#a5a5a5', '#f5ff8a'];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        popup.style.color = randomColor;
+    }
+
+    setInterval(changeColor, 1000);
 });
+
+function redirectToNextPage() {
+    window.location.href = 'next.html';
+}
